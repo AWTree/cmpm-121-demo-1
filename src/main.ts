d65916b5c26e9ae84eb7242921516a33a4bef9fd
AWTree <arnowu320@gmail.com>
2024-10-07 12:19:53 -0700

    Automatic Clicking

--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,19 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+/*
+function boost(score: number) {
+    return score + 1 + + Math.sqrt(score);  // Increment the score by 1
+  }
+*/
+
 let num_clicks = 0;
 const button = document.createElement("button");
 button.innerHTML = "Click!";
-button.onclick = () => {
-  num_clicks += 1;
-  button.innerHTML = `${num_clicks} clicks so far`;
-};
+setInterval(() => {
+    num_clicks += 1;
+    button.innerHTML = `${num_clicks.toFixed(2)} clicks so far`; // Automatic Clicking
+}, 1000); 
 app.append(button);
+
+
